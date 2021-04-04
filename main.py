fc5b3cd8645cbacf0f4ecb99ea15c31c739af03d
Warren Wei <warrenwei1108@gmail.com>
2021-04-05 08:42:31 +1000

    Update

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api, Resource 
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+  def get(self):
+    return {"data": "Hello World"}
+  def post(self):
+    return {"data": "posted"}
+    
+api.add_resource(HelloWorld, "/helloworld")
+
+if __name__ == "__main__":
+  app.run(debug=True)
