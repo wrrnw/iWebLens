23a6e72692459d5e3de062113871b87e7bc7783c
Warren Wei <warrenwei1108@gmail.com>
2021-04-05 16:51:16 +1000

    Get image id from request

--- iWebLens_server.py
+++ iWebLens_server.py
@@ -1,14 +1,25 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource, reqparse, abort
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 
+# @app.route('/api/object_detection')
+# def image():
+#   if request.method == "POST":
+#     data = request.form
+#     return request.form
+
 class Image(Resource):
   def post(self):
-    data = request.get_json()
-    return data
+    image_id = json.loads(request.json)['id']
+    #result = json.dumps('{"id": {}, "object": [{"label": "book"}]}'.format(image_id))
+    #image = Image.open(ByteIO(i))
+    #image = np.array(image)
+          #request.get_json()
+    return image_id
 
 
 api.add_resource(Image, "/api/object_detection")
